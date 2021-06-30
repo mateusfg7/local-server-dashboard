43fc4d796836d64aee79472a42a0b0a5a7cee5ba
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-06-30 14:22:28 -0300

    web-ui: initial project

---
+++ web-ui/src/pages/index.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100vw',
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1>Home</h1>
+    </div>
+  );
+}
