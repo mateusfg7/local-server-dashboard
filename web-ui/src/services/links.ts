e000ea77f57e1aa2c8f6f9c44c21d6e1fa3304a1
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-06-30 17:43:33 -0300

    web-ui: create links

---
+++ web-ui/src/services/links.ts
@@ -0,0 +1,14 @@
+const links = [
+  {
+    service: 'portainer',
+    title: 'Portainer',
+    url: 'http://127.0.0.1:9000'
+  },
+  {
+    service: 'emby',
+    title: 'Emby',
+    url: 'http://127.0.0.1:8097'
+  }
+]
+
+export default links
