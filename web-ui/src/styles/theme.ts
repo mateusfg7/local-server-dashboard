f45066ccbe6ee1ab73f2f06bce2acf09071c2ecb
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-07-05 18:35:09 -0300

    web-ui: update theme

--- web-ui/src/styles/theme.ts
+++ web-ui/src/styles/theme.ts
@@ -3,6 +3,10 @@ const defaultTheme = {
     background: 'rgb(30, 28, 33)',
     section: 'rgb(51, 49, 56)',
     text: '#fff'
+  },
+  assets: {
+    backgroundImage:
+      'https://bipedepop.files.wordpress.com/2018/08/bc3adpede-pop-para-reflexc3a3o-e-relaxamento-conhec3a7a-lofi-hip-hop-um-estilo-para-introspectividade-e-para-vida-2.gif?w=1200'
   }
 }
 
