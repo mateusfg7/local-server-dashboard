794439a9c9a94cab6b99bbeb603ec8be128d844d
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-07-05 18:36:12 -0300

    web-ui: update startpage

--- web-ui/src/pages/index.tsx
+++ web-ui/src/pages/index.tsx
@@ -1,45 +1,123 @@
-import React, { useEffect } from 'react'
+/* eslint-disable camelcase */
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
+import { format } from 'date-fns'
 
-import links from '../services/links'
+import weatherApi from '../services/api'
+import hostList from '../services/hosts.json'
 
-import LinkCard from '../components/LinkCard'
+import { Container, Main, Clock, Content, Host } from '../styles/pages/index'
+import { GetServerSideProps } from 'next'
 
-import { Main, Header, Content } from '../styles/pages/index'
+interface WeatherCurrent {
+  location: {
+    name: string
+    region: string
+    country: string
+    lat: number
+    lon: number
+    tz_id: string
+  }
+  current: {
+    last_updated: string
+    temp_c: number
+    is_day: number
+    condition: {
+      text: string
+      icon: string
+      humidity: number
+    }
+  }
+}
 
-const Home: React.FC = () => {
+interface Props {
+  weatherData: WeatherCurrent
+}
+
+const Home: React.FC<Props> = ({ weatherData }) => {
+  const [currentDate, setCurrentDate] = useState(new Date())
+  const [search, setSearch] = useState<string>()
+
+  useEffect(() => {
+    setInterval(() => {
+      setCurrentDate(new Date())
+    }, 500)
+  }, [])
+
+  function handleSearch(e: React.FormEvent) {
+    e.preventDefault()
+
+    alert(search)
+
+    window.location.replace(`https://anon.sx/search?q=${search}`)
+  }
+
   return (
     <div>
       <Head>
         <title>L.S.D.</title>
       </Head>
 
-      <Main>
-        <Header>
-          <div>
-            <h1>L.S.D.</h1>
-            <p>Local Server Dashboard</p>
-          </div>
-          <div>
-            <h2>Hora</h2>
-            <h2>Clima</h2>
-          </div>
-        </Header>
-
-        <Content>
-          <div className="cards">
-            {links.map(service => (
-              <LinkCard
-                key={service.service}
-                url={service.url} background={service.mainColor}>
-                {service.title}
-              </LinkCard>
-            ))}
-          </div>
-        </Content>
-      </Main>
+      <Container>
+        <Main>
+          <Clock>
+            <div className="hour">
+              <p>{format(currentDate, 'p')}</p>
+            </div>
+            <div className="weather">
+              <p>{weatherData.current.temp_c}°</p>
+              <Image
+                src={`https:${weatherData.current.condition.icon}`}
+                width={70}
+                height={70}
+              />
+            </div>
+          </Clock>
+          <Content>
+            <header>
+              <h1>Welcome, Mateus</h1>
+              <p>Today is {format(currentDate, 'EEEE, LLLL d')}</p>
+            </header>
+            <div className="services">
+              {hostList.map(host => (
+                <Host
+                  href={host.url}
+                  key={host.service}
+                  target="_blank"
+                  rel="noreferrer"
+                  color={host.mainColor}
+                >
+                  <div>{host.title}</div>
+                </Host>
+              ))}
+            </div>
+            <div className="search">
+              <form onSubmit={handleSearch}>
+                <input
+                  type="search"
+                  name="search"
+                  value={search}
+                  onChange={e => setSearch(e.target.value)}
+                />
+              </form>
+            </div>
+          </Content>
+        </Main>
+      </Container>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const weatherRequest = await weatherApi.get('current.json')
+  const weatherData: WeatherCurrent = weatherRequest.data
+
+  return {
+    props: {
+      weatherData
+    }
+  }
+}
--- web-ui/src/styles/pages/index.ts
+++ web-ui/src/styles/pages/index.ts
@@ -1,34 +1,101 @@
 import styled from 'styled-components'
 
-export const Main = styled.main`
-  display: flex;
-  flex-direction: column;
+interface ContentProps {
+  color: string
+}
 
-  min-height: 100vh;
-  width: min-content;
-  min-width: 40rem;
-
-  margin: 0 auto;
-`
-
-export const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  padding: 1rem;
-`
-
-export const Content = styled.div`
-  flex: 1;
-
+export const Container = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  .cards {
+  width: 100vw;
+  height: 100vh;
+  background: url(${props => props.theme.assets.backgroundImage}) no-repeat
+    center;
+  background-size: cover;
+`
+
+export const Main = styled.main`
+  display: flex;
+
+  border-radius: 10px;
+
+  box-shadow: 0 0 40px black;
+`
+
+export const Clock = styled.section`
+  padding: 1rem;
+
+  font-size: 1.5rem;
+  border-radius: 10px 0 0 10px;
+
+  background: url(${props => props.theme.assets.backgroundImage}) no-repeat;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+export const Content = styled.div`
+  padding: 1rem;
+  border-radius: 0 10px 10px 0;
+
+  background: ${props => props.theme.colors.background};
+
+  header {
+    h1 {
+      font-size: 1.5rem;
+    }
+  }
+
+  div.services {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    gap: 20px;
+    padding: 2rem 0;
+    gap: 0.5rem;
+    width: min-content;
+  }
+
+  div.search {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+
+    form {
+      flex: 1;
+      input {
+        width: 100%;
+        background: none;
+        border: none;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+
+        font-size: 1rem;
+        color: white;
+
+        padding: 0.3rem;
+
+        &:hover,
+        &:focus {
+          border-bottom: 1px solid white;
+        }
+      }
+    }
+  }
+`
+
+export const Host = styled.a<ContentProps>`
+  color: white;
+
+  div {
+    width: max-content;
+    padding: 0.3rem;
+    transition: 0.2s;
+    border-bottom: 1px solid transparent;
+    &:hover {
+      border-bottom: 1px solid white;
+    }
   }
 `
