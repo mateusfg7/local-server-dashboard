c9a996bdbdf0c5bb9b7544c091695f1605921733
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-07-08 10:32:07 -0300

    web-ui: use getCurrentWeatherData instead axios api request

--- web-ui/src/pages/index.tsx
+++ web-ui/src/pages/index.tsx
@@ -1,41 +1,22 @@
-/* eslint-disable camelcase */
 import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import { format } from 'date-fns'
 
-import weatherApi from '../services/api'
+import {
+  getCurrentWeatherData,
+  CurrentWeatherData
+} from '../utils/getWeatherData'
 import hostList from '../services/hosts.json'
 
 import { Container, Main, Clock, Content, Host } from '../styles/pages/index'
 import { GetServerSideProps } from 'next'
 
-interface WeatherCurrent {
-  location: {
-    name: string
-    region: string
-    country: string
-    lat: number
-    lon: number
-    tz_id: string
-  }
-  current: {
-    last_updated: string
-    temp_c: number
-    is_day: number
-    condition: {
-      text: string
-      icon: string
-      humidity: number
-    }
-  }
-}
-
 interface Props {
-  weatherData: WeatherCurrent
+  currentWeatherData: CurrentWeatherData
 }
 
-const Home: React.FC<Props> = ({ weatherData }) => {
+const Home: React.FC<Props> = ({ currentWeatherData }) => {
   const [currentDate, setCurrentDate] = useState(new Date())
   const [search, setSearch] = useState<string>()
 
@@ -64,9 +45,9 @@ const Home: React.FC<Props> = ({ weatherData }) => {
               <p>{format(currentDate, 'p')}</p>
             </div>
             <div className="weather">
-              <p>{weatherData.current.temp_c}°</p>
+              <p>{currentWeatherData.current.temp_c}°</p>
               <Image
-                src={`https:${weatherData.current.condition.icon}`}
+                src={`https:${currentWeatherData.current.condition.icon}`}
                 width={70}
                 height={70}
               />
@@ -110,12 +91,11 @@ const Home: React.FC<Props> = ({ weatherData }) => {
 export default Home
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const weatherRequest = await weatherApi.get('current.json')
-  const weatherData: WeatherCurrent = weatherRequest.data
+  const currentWeatherData = await getCurrentWeatherData()
 
   return {
     props: {
-      weatherData
+      currentWeatherData
     }
   }
 }
